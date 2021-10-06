3bab59dbf2f3e1b364da7aab70775a88d9b1857c
Dasha <you@example.com>
2021-10-06 21:47:20 +0300

    DashaLib added

--- Test_Git.pro
+++ Test_Git.pro
@@ -4,8 +4,10 @@ CONFIG -= app_bundle
 CONFIG -= qt
 
 SOURCES += \
+        LibDasha.cpp \
         MyLib01.cpp \
         main.cpp
 
 HEADERS += \
+    LibDasha.h \
     MyLib01.h
--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "MyLib01.h"
+#include "LibDasha.h"
 
 using namespace std;
 
@@ -7,6 +8,9 @@ int main()
 {
     PrintSergey_01();
     PrintSergey_02();
+
+    PrintDasha_01();
+
 
     return 0;
 }
